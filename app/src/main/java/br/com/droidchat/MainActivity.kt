5525a493f565809d23d4485c084a7a3ac57ae585
ronanlm10@hotmail.com <ronanlm10@hotmail.com.br>
2024-07-16 20:42:14 -0300

    feat: Impl navigation host

--- app/src/main/java/br/com/droidchat/MainActivity.kt
+++ app/src/main/java/br/com/droidchat/MainActivity.kt
@@ -3,13 +3,7 @@ package br.com.droidchat
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import br.com.droidchat.navigation.ChatNavHost
 import br.com.droidchat.ui.theme.DroidChatTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,32 +12,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DroidChatTheme {
-                Scaffold(Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ChatNavHost()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(
-    name: String,
-    modifier: Modifier = Modifier
-) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DroidChatTheme {
-        Greeting("Android")
     }
 }
