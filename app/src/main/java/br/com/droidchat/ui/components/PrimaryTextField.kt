0b19f6ddf40ea32a939aea42596518f56e4c0365
Ronan Fernandes <ronanlm10@hotmail.com>
2025-02-06 16:38:29 -0300

    feat: Impl primary text field

---
+++ app/src/main/java/br/com/droidchat/ui/components/PrimaryTextField.kt
@@ -0,0 +1,117 @@
+package br.com.droidchat.ui.components
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.droidchat.R
+import br.com.droidchat.ui.theme.DroidChatTheme
+
+@Composable
+fun PrimaryTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    @DrawableRes leadingIconRes: Int? = null,
+    placeHolder: String = "",
+    errorMessage: String? = null,
+    singleLine: Boolean = true,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    imeAction: ImeAction = ImeAction.Next,
+) {
+    val isKeyBoardTypePassword = keyboardType == KeyboardType.Password
+    var passwordVisibility by remember { mutableStateOf(false) }
+
+    val trailingIcon = if (passwordVisibility) R.drawable.ic_visibility
+    else R.drawable.ic_visibility_off
+
+    Column(modifier = modifier) {
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = value,
+            onValueChange = onValueChange,
+            trailingIcon = {
+                if (isKeyBoardTypePassword && value.isNotEmpty()) {
+                    Icon(
+                        modifier = Modifier.clickable { passwordVisibility = !passwordVisibility },
+                        painter = painterResource(trailingIcon),
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                }
+            },
+            leadingIcon = {
+                leadingIconRes?.let { icon ->
+                    Icon(
+                        painter = painterResource(icon),
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                }
+            },
+            singleLine = singleLine,
+            visualTransformation = if (isKeyBoardTypePassword) {
+                if (passwordVisibility) VisualTransformation.None
+                else PasswordVisualTransformation()
+            } else VisualTransformation.None,
+            placeholder = { Text(text = placeHolder) },
+            shape = CircleShape,
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+                focusedContainerColor = MaterialTheme.colorScheme.surface,
+                disabledContainerColor = MaterialTheme.colorScheme.surface,
+                unfocusedBorderColor = if (errorMessage != null) MaterialTheme.colorScheme.error
+                else MaterialTheme.colorScheme.onSurfaceVariant,
+                focusedBorderColor = MaterialTheme.colorScheme.primary
+            ),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = keyboardType,
+                imeAction = imeAction
+            )
+        )
+
+        errorMessage?.let {
+            Text(
+                text = it,
+                modifier = Modifier.padding(4.dp),
+                color = MaterialTheme.colorScheme.error
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    DroidChatTheme {
+        PrimaryTextField(
+            leadingIconRes = R.drawable.ic_envelope,
+            value = "123445",
+            errorMessage = "Senha é obrigatória",
+            keyboardType = KeyboardType.Password,
+            placeHolder = "E-Mail",
+            onValueChange = {}
+        )
+    }
+}
--- app/src/main/java/br/com/droidchat/ui/feature/signin/SigInScreen.kt
+++ app/src/main/java/br/com/droidchat/ui/feature/signin/SigInScreen.kt
@@ -4,13 +4,24 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.droidchat.R
+import br.com.droidchat.ui.components.PrimaryTextField
 import br.com.droidchat.ui.theme.BackgroundGradient
 import br.com.droidchat.ui.theme.DroidChatTheme
 
@@ -31,6 +42,32 @@ fun SignInScreen() {
         Image(
             painter = painterResource(R.drawable.logo),
             contentDescription = null
+        )
+
+        Spacer(Modifier.height(64.dp))
+
+        var email by remember { mutableStateOf("") }
+
+        PrimaryTextField(
+            modifier = Modifier.padding(horizontal = 16.dp),
+            value = email,
+            onValueChange = { email = it },
+            keyboardType = KeyboardType.Email,
+            placeHolder = stringResource(R.string.feature_login_email),
+            leadingIconRes = R.drawable.ic_envelope
+        )
+
+        Spacer(Modifier.height(16.dp))
+
+        var password by remember { mutableStateOf("") }
+
+        PrimaryTextField(
+            modifier = Modifier.padding(horizontal = 16.dp),
+            value = password,
+            onValueChange = { password = it },
+            keyboardType = KeyboardType.Password,
+            placeHolder = stringResource(R.string.feature_login_password),
+            leadingIconRes = R.drawable.ic_lock
         )
     }
 }
